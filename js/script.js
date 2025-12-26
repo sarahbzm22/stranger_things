e53a30f0efa639d48dc953af8ba58e7549be5e4e
strawberrybliss <lucisafl78@gmail.com>
2025-12-26 23:25:22 +0100

    upside down home en proceso

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const pantallaCarga = document.getElementById("pantalla-carga");
-  const video = pantallaCarga.querySelector("video");
-  const menu = document.getElementById("menu");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const pantallaCarga = document.getElementById("pantalla-carga");
+//   const video = pantallaCarga.querySelector("video");
+//   const menu = document.getElementById("menu");
 
-  // Bloqueamos scroll al inicio
-  document.body.classList.add("no-scroll");
+//   // Bloqueamos scroll al inicio
+//   document.body.classList.add("no-scroll");
 
-  if (!video || !pantallaCarga) return;
+//   if (!video || !pantallaCarga) return;
 
-  video.addEventListener("ended", () => {
-    // Ocultamos pantalla de carga
-    pantallaCarga.classList.add("ocultar");
+//   video.addEventListener("ended", () => {
+//     // Ocultamos pantalla de carga
+//     pantallaCarga.classList.add("ocultar");
 
-    // Mostramos el menú
-    menu.style.opacity = "1";
-    menu.style.pointerEvents = "auto";
+//     // Mostramos el menú
+//     menu.style.opacity = "1";
+//     menu.style.pointerEvents = "auto";
 
-    // Quitamos el bloqueo de scroll
-    document.body.classList.remove("no-scroll");
+//     // Quitamos el bloqueo de scroll
+//     document.body.classList.remove("no-scroll");
 
-    // Eliminamos el section tras la animación
-    setTimeout(() => {
-      pantallaCarga.style.display = "none";
-    }, 1000);
-  });
-});
+//     // Eliminamos el section tras la animación
+//     setTimeout(() => {
+//       pantallaCarga.style.display = "none";
+//     }, 1000);
+//   });
+// });
 
