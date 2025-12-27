e128925a38370f1239fbeebf77ae3be73ca672a3
sarahbzm22 <sbzm22abril@gmail.com>
2025-12-27 03:33:33 +0100

    Añadido el botón que inicia la pantalla de carga y el audio

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const pantallaCarga = document.getElementById("pantalla-carga");
-//   const video = pantallaCarga.querySelector("video");
-//   const menu = document.getElementById("menu");
+document.addEventListener("DOMContentLoaded", () => {
+  const prePantalla = document.getElementById("pre-pantalla-carga");
+  const pantallaCarga = document.getElementById("pantalla-carga");
+  const video = pantallaCarga.querySelector("video");
+  const menu = document.getElementById("menu");
+  const boton = document.getElementById("btn-play");
+  const audio = document.getElementById("st-audio");
 
-//   // Bloqueamos scroll al inicio
-//   document.body.classList.add("no-scroll");
+  // Bloqueamos scroll al inicio
+  document.body.classList.add("no-scroll");
 
-//   if (!video || !pantallaCarga) return;
+  // El vídeo NO debe arrancar solo
+  video.pause();
 
-//   video.addEventListener("ended", () => {
-//     // Ocultamos pantalla de carga
-//     pantallaCarga.classList.add("ocultar");
+  boton.addEventListener("click", () => {
+    // Ocultamos la pre-pantalla
+    prePantalla.style.opacity = "0";
+    prePantalla.style.pointerEvents = "none";
 
-//     // Mostramos el menú
-//     menu.style.opacity = "1";
-//     menu.style.pointerEvents = "auto";
+    setTimeout(() => {
+      prePantalla.style.display = "none";
+    }, 1000);
 
-//     // Quitamos el bloqueo de scroll
-//     document.body.classList.remove("no-scroll");
+    // Arrancamos vídeo y audio A LA VEZ
+    video.play();
+    audio.play();
+  });
 
-//     // Eliminamos el section tras la animación
-//     setTimeout(() => {
-//       pantallaCarga.style.display = "none";
-//     }, 1000);
-//   });
-// });
+  video.addEventListener("ended", () => {
+    // Ocultamos pantalla de carga
+    pantallaCarga.classList.add("ocultar");
 
+    // Mostramos el menú
+    menu.classList.add("visible");
+
+    // Quitamos bloqueo de scroll
+    document.body.classList.remove("no-scroll");
+
+    // Eliminamos el section tras la animación
+    setTimeout(() => {
+      pantallaCarga.style.display = "none";
+    }, 1000);
+  });
+});
